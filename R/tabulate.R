668b1021b69cc10966bf86ffc7f1a89ddcd20f28
Kurt Hornik <Kurt.Hornik@R-project.org>
2007-07-10 19:13:12 +0000

    version 0.3-18

--- R/tabulate.R
+++ R/tabulate.R
@@ -3,5 +3,10 @@ function(x)
 {
     values <- unique(x)
     counts <- tabulate(match(x, values))
-    data.frame(values = I(values), counts = counts)
+    ## Still a bit tricky to create a data frame with a list "column"
+    ## which is not protected by I(); otherwise, we oculd simply do
+    ##   data.frame(values = I(values), counts = counts)
+    out <- data.frame(values = double(length(values)), counts = counts)
+    out$values <- values
+    out
 }
