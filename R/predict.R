95391d780d427581e7fe8636e345cc19677ec27a
Kurt Hornik <Kurt.Hornik@R-project.org>
2018-04-23 08:11:19 +0000

    version 0.3-55

--- R/predict.R
+++ R/predict.R
@@ -64,7 +64,7 @@ function(object, newdata = NULL,
     method <- object$call$metric
     if(is.null(method)) {
         ## Not given in the call, hence use default value.
-        method <- formals(cluster::clara)$metric
+        method <- eval(formals(cluster::clara)$metric)[1L]
         ## (Or hard-wire the default value: "euclidean".)
     }
     d <- .rxdist(newdata, object$medoids, method)
@@ -88,7 +88,7 @@ function(object, newdata = NULL,
     method <- object$call$metric
     if(is.null(method)) {
         ## Not given in the call, hence use default value.
-        method <- formals(cluster::pam)$metric
+        method <- eval(formals(cluster::pam)$metric)[1L]
         ## (Or hard-wire the default value: "euclidean".)
     }
     d <- .rxdist(newdata, object$medoids, method)
